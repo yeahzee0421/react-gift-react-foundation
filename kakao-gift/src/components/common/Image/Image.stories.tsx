fcc8707312554739c141c105d27a3342513d1b72
yeahzee0421 <ydw08250421@gmail.com>
2024-06-28 05:15:58 +0900

    feat: add Image component

---
+++ kakao-gift/src/components/common/Image/Image.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Meta, StoryFn } from '@storybook/react';
+import Image, { ImageProps } from './Image';
+
+export default {
+  title: 'Image',
+  component: Image,
+  tags: ['autodocs'],
+} as Meta;
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Template: StoryFn<ImageProps> = (args) => <Image {...args} />;
+const imageUrl = 'https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png';
+
+export const RatioSquare = Template.bind({});
+RatioSquare.args = {
+  ratio: 'square',
+  src: imageUrl,
+  width: '100px',
+};
+
+export const RadiusCircle = Template.bind({});
+RadiusCircle.args = {
+  radius: 'circle',
+  ratio: 'square',
+  src: imageUrl,
+  width: '100px',
+};
---
+++ kakao-gift/src/components/common/Image/Image.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export interface ImageProps {
+  src?: string;
+  width?: string;
+  ratio?: number | 'square';
+  radius?: number | 'circle';
+}
+
+const getRadius = (radius?: number | 'circle'): string | undefined => {
+  if (radius === 'circle') {
+    return '50%';
+  } if (typeof radius === 'number') {
+    return `${radius}px`;
+  }
+  return undefined;
+};
+
+const Image: React.FC<ImageProps> = ({
+  src, width, ratio, radius,
+}) => {
+  let heightStyle: React.CSSProperties = {};
+
+  if (ratio === 'square') {
+    heightStyle = {
+      width,
+      height: width,
+      objectFit: 'cover',
+      borderRadius: getRadius(radius),
+    };
+  } else if (typeof ratio === 'number' && ratio > 0) {
+    const paddingBottom = `${(1 / ratio) * 100}%`;
+    heightStyle = {
+      width,
+      paddingBottom,
+      position: 'relative',
+      overflow: 'hidden',
+      borderRadius: getRadius(radius),
+    };
+  }
+
+  return <img src={src} style={heightStyle} alt="imageSample" />;
+};
+
+export default Image;
