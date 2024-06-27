48470c6f6a278d05cf10131857643545a1b9e8c8
yeahzee0421 <ydw08250421@gmail.com>
2024-06-28 03:52:17 +0900

    feat: add button component

---
+++ kakao-gift/src/components/common/Button.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Meta, StoryFn } from '@storybook/react';
+import Button, { ButtonProps } from './Button';
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    theme: {
+      control: {
+        type: 'select',
+        options: ['kakao', 'default'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'large', 'responsive'],
+      },
+    },
+  },
+} as Meta;
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const Template: StoryFn<ButtonProps> = (args) => <Button {...args} />;
+
+export const Kakao = Template.bind({});
+Kakao.args = {
+  theme: 'kakao',
+  size: 'responsive',
+  children: '버튼',
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  theme: 'default',
+  size: 'responsive',
+  children: '버튼',
+};
---
+++ kakao-gift/src/components/common/Button.tsx
@@ -0,0 +1,17 @@
+// src/Button.tsx
+import React from 'react';
+import './Button.css';
+
+export interface ButtonProps {
+  theme: 'kakao' | 'default';
+  size: 'small' | 'large' | 'responsive';
+  children: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ theme, size, children }) => (
+  <button className={`button ${theme} ${size}`} type="button">
+    {children}
+  </button>
+);
+
+export default Button;
